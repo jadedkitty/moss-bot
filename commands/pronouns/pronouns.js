b638723d97f7001eaee873f2c1899c3019a4ce68
Emma Maguire <emma@emmalyndesign.com>
2021-11-16 17:24:51 -0700

    Added pronouns commands

--- commands/pronouns/pronouns.js
+++ commands/pronouns/pronouns.js
@@ -1,26 +1,83 @@
 /**
- * @file Sample ping command
- * @author Naman Vrati
+ * @file Pronoun command
+ * @author Emma Maguire
  * @since 1.0.0
  */
 
 module.exports = {
-	name: "ping",
+  name: "pronouns",
+  description: "Adds/sets the pronouns role for the sender",
+  args: true,
 
-	/** You need to uncomment below properties if you need them. */
-	//description: 'Ping!',
-	//usage: 'put usage here',
-	//permissions: 'SEND_MESSAGES',
-	//guildOnly: true,
+  /** You need to uncomment below properties if you need them. */
+  //description: 'Adds pronouns for user',
+  usage: "add/remove <pronouns>",
+  //permissions: 'SEND_MESSAGES',
+  //guildOnly: true,
 
-	/**
-	 * @description Executes when the command is called by command handler.
-	 * @author Naman Vrati
-	 * @param {Object} message The Message Object of the command.
-	 * @param {String[]} args The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
-	 */
+  /**
+   * @description Executes when the command is called by command handler.
+   * @author Naman Vrati
+   * @param {Object} message The Message Object of the command.
+   * @param {String[]} args The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
+   */
 
-	execute(message, args) {
-		message.channel.send({ content: "Pong." });
-	},
+  execute(message, args) {
+    if (args[0] == "add") {
+      if (args[1]) {
+        var roleName = args[1].toLowerCase().toString();
+        var toRole = message.guild.roles.cache.find((role) =>
+          role.name.toLowerCase().includes(args[1].toLowerCase())
+        );
+        if (toRole) {
+          if (message.member.roles.cache.has(toRole.id)) {
+            message.reply("You already have the role `" + toRole.name + "`!");
+          } else {
+            message.member.roles.add(toRole).catch(console.error);
+            message.reply(
+              "`" + toRole.name + "` has been added to your pronouns."
+            );
+          }
+        } else {
+          console.log(roleName);
+          message.guild.roles
+            .create({
+              name: roleName,
+              reason: "Added pronouns",
+            })
+            .catch(console.error)
+            .then((role) => {
+              message.member.roles.add(role);
+              message.reply(
+                "`" + role.name + "` has been added to your pronouns."
+              );
+            });
+        }
+      } else {
+        message.reply(
+          "Please specify the pronoun that you would like to set for yourself!"
+        );
+      }
+    }
+    if (args[0] == "remove") {
+      console.log(args[1].toLowerCase());
+      if (args[1]) {
+        var toRole = message.guild.roles.cache.find((role) =>
+          role.name.toLowerCase().includes(args[1].toLowerCase())
+        );
+        if (toRole) {
+          if (message.member.roles.cache.has(toRole.id)) {
+            message.member.roles.remove(toRole).catch(console.error);
+            message.reply(
+              "`" + toRole.name + "` has been removed from your pronouns!"
+            );
+          } else {
+            message.reply("You don't have the role `" + toRole.name + "`!");
+          }
+        } else {
+          message.reply("That role doesn't exist!");
+        }
+      }
+    }
+  },
 };
