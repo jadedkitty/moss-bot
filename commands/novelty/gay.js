41ed295f076a9b1a342cf8cb115818c85eac3db6
Emma Maguire <emma@emmalyndesign.com>
2021-11-18 03:30:03 -0700

    Added gay command

---
+++ commands/novelty/gay.js
@@ -0,0 +1,32 @@
+/**
+ * @file Gay command
+ * @author Emma Maguire
+ * @since 1.0.0
+ */
+
+module.exports = {
+  name: "gay",
+
+  /** You need to uncomment below properties if you need them. */
+  description: "Makes your avatar gayer",
+  // usage: '<user>',
+  //permissions: 'SEND_MESSAGES',
+  // guildOnly: true,
+
+  /**
+   * @description Executes when the command is called by command handler.
+   * @author Emma Maguire
+   * @param {Object} message The Message Object of the command.
+   * @param {String[]} args The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
+   */
+
+  execute(message, args) {
+    const fetch = require("node-fetch");
+    fetch("https://some-random-api.ml/canvas/gay?avatar="+ message.author.displayAvatarURL({format: 'png'}))
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        message.channel.send(json.link);
+      });
+  },
+};
