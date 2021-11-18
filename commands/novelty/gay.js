04119b95dccf1f0edc3f5f40e66bd4ce8c7f4555
Emma Maguire <emma@emmalyndesign.com>
2021-11-18 03:55:10 -0700

    Update gay.js

--- commands/novelty/gay.js
+++ commands/novelty/gay.js
@@ -21,14 +21,14 @@ module.exports = {
    */
 
   execute(message, args) {
-    var url =
-      "https://some-random-api.ml/canvas/gay?avatar=" +
-      message.author.displayAvatarURL({ format: "jpg" });
     min = Math.ceil(1);
     max = Math.floor(100);
     var randomNumber = Math.floor(Math.random() * (max - min) + min);
-    message.channel.send("U R NOW " + randomNumber + "% MORE GAY", {
-      file: url,
-    });
+    message.reply(
+      "U R NOW " +
+        randomNumber +
+        "% MORE GAY https://some-random-api.ml/canvas/gay?avatar=" +
+        message.author.displayAvatarURL({ format: "jpg" })
+    );
   },
 };
